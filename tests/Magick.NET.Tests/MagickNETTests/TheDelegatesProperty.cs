b6bb2b4a1a4aa4edf93b62966bb5c94155f14b3b
Dirk Lemstra <dirk@lemstra.org>
2026-09-26 19:55:19 +0200

    Corrected unit test because support for zstd was added.

--- tests/Magick.NET.Tests/MagickNETTests/TheDelegatesProperty.cs
+++ tests/Magick.NET.Tests/MagickNETTests/TheDelegatesProperty.cs
@@ -15,9 +15,9 @@ public partial class MagickNETTests
             var delegates = MagickNET.Delegates;
 
             if (Runtime.IsWindows)
-                Assert.Equal("bzlib cairo freetype gslib heic jng jp2 jpeg jxl lcms lqr lzma openexr pangocairo png ps raqm raw rsvg tiff webp xml zip zlib", delegates);
+                Assert.Equal("bzlib cairo freetype gslib heic jng jp2 jpeg jxl lcms lqr lzma openexr pangocairo png ps raqm raw rsvg tiff webp xml zip zlib zstd", delegates);
             else
-                Assert.Equal("bzlib cairo fontconfig freetype heic jng jp2 jpeg jxl lcms lqr lzma openexr pangocairo png raqm raw rsvg tiff webp xml zip zlib", delegates);
+                Assert.Equal("bzlib cairo fontconfig freetype heic jng jp2 jpeg jxl lcms lqr lzma openexr pangocairo png raqm raw rsvg tiff webp xml zip zlib zstd", delegates);
         }
     }
 }
